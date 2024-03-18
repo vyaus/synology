2ed1990cb3baeec296d06a7ee770e1c1f4a27cc9
vyaus <140357659+vyaus@users.noreply.github.com>
2024-03-18 10:34:05 +0800

    add group admin only from groups with more than 2 members

--- Readme.md
+++ Readme.md
@@ -2,10 +2,8 @@
 
 The format of list.txt is as follow:
 
- - 1ne line for a group
- - the first field is group name
- - the second is group admin
- - the third to last field is group member
+ - one line for a group
+ - the first field is group name, the second is group admin, the third to last is group members
 
 `create_groups_users_shares.py` can:
 
@@ -14,3 +12,5 @@ The format of list.txt is as follow:
  - set proper permissions for users and groups
 
 Group admin can read/write all members' shared folder, while members can only read each other's shared folders within the same groups.
+
+Group admin only exist for groups with more than 2 members.
--- create_groups_users_shares.py
+++ create_groups_users_shares.py
@@ -39,8 +39,11 @@ def create_user(username, group):
 def create_group(groupname, members):
    print(f'synogroup --add "{groupname}" {members}')
 
-def create_share(groupname, member, admin):
-   print(f'synoshare --add "{groupname} - {member}" "{groupname}" "/volume1/{groupname} - {member}" "" "@{admin},{member}" "@{groupname}" 1 0')
+def create_share(groupname, member, admin=None):
+    if admin is not None:
+       print(f'synoshare --add "{groupname} - {member}" "{groupname}" "/volume1/{groupname} - {member}" "" "@{admin},{member}" "@{groupname}" 1 0')
+    else:
+       print(f'synoshare --add "{groupname} - {member}" "{groupname}" "/volume1/{groupname} - {member}" "" "{member}" "@{groupname}" 1 0')
 
 with open('list.txt', 'r') as f:
    reader = csv.reader(f, delimiter=' ')
@@ -51,7 +54,11 @@ with open('list.txt', 'r') as f:
        for username in members:
            create_user(username, group)
        create_group(group, ' '.join(members))
-       create_group(group + "admin", admin)
-       for member in members:
-           create_share(group, member, group + "admin")
+       if len(members) > 2:
+           create_group(group + "admin", admin)
+           for member in members:
+               create_share(group, member, group + "admin")
 
+       else:
+           for member in members:
+               create_share(group, member)
